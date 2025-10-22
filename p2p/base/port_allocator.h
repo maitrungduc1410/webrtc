09b610b6c299562c950b8f3a6c7cb90fd50f82a9
Harald Alvestrand <hta@webrtc.org>
2025-10-22 10:06:34 +0000

    Move implementation of SubscribePortReady to the .cc file
    This avoids a linker error in Chromium.
    
    Bug: webrtc:42222066
    Change-Id: Ib1c6b66b550c6b71825fe3b517e2ed3a0a20d13d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/419360
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
    Commit-Queue: Harald Alvestrand <hta@webrtc.org>
    Auto-Submit: Harald Alvestrand <hta@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#46012}

--- p2p/base/port_allocator.h
+++ p2p/base/port_allocator.h
@@ -267,10 +267,9 @@ class RTC_EXPORT PortAllocatorSession : public sigslot::has_slots<> {
   virtual void PruneAllPorts() {}
 
   sigslot::signal2<PortAllocatorSession*, PortInterface*> SignalPortReady;
-  void SubscribePortReady(absl::AnyInvocable<void(PortAllocatorSession*,
-                                                  PortInterface*)> callback) {
-    port_ready_trampoline_.Subscribe(std::move(callback));
-  }
+  // This function has to be non-inlined due to usage in Chrome.
+  void SubscribePortReady(
+      absl::AnyInvocable<void(PortAllocatorSession*, PortInterface*)> callback);
   void NotifyPortReady(PortAllocatorSession* session, PortInterface* port) {
     SignalPortReady(session, port);
   }
