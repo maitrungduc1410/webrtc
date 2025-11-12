d471f7cf9a3e7900404ba5ef14d42fb39b063766
Tommi <tommi@webrtc.org>
2025-11-12 14:33:20 +0100

    Use absl::AnyInvocable instead of std::function in ChannelInterface
    This change updates `ChannelInterface` to use `absl::AnyInvocable<void()
    &&>` instead of `std::function<void()>` for the first packet
    received/sent callbacks.
    
    This aligns with webrtc guidance:
    https://webrtc.googlesource.com/src/+/refs/heads/main/g3doc/style-guide.md
    
    No-Iwyu: Verified iwyu locally. The iwyu bot is acting up.
    Bug: none
    Change-Id: I2818ff6d9537b1779e4080ed392dab7efc2898be
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/425020
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#46170}

--- pc/channel.h
+++ pc/channel.h
@@ -13,13 +13,13 @@
 
 #include <stdint.h>
 
-#include <functional>
 #include <memory>
 #include <optional>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "absl/functional/any_invocable.h"
 #include "absl/strings/string_view.h"
 #include "api/crypto/crypto_options.h"
 #include "api/jsep.h"
@@ -148,8 +148,10 @@ class BaseChannel : public ChannelInterface,
   }
 
   // Used for latency measurements.
-  void SetFirstPacketReceivedCallback(std::function<void()> callback) override;
-  void SetFirstPacketSentCallback(std::function<void()> callback) override;
+  void SetFirstPacketReceivedCallback(
+      absl::AnyInvocable<void() &&> callback) override;
+  void SetFirstPacketSentCallback(
+      absl::AnyInvocable<void() &&> callback) override;
 
   // From RtpTransport - public for testing only
   void OnTransportReadyToSend(bool ready);
@@ -316,9 +318,10 @@ class BaseChannel : public ChannelInterface,
   scoped_refptr<PendingTaskSafetyFlag> alive_;
 
   // The functions are deleted after they have been called.
-  std::function<void()> on_first_packet_received_
+  absl::AnyInvocable<void() &&> on_first_packet_received_
       RTC_GUARDED_BY(network_thread());
-  std::function<void()> on_first_packet_sent_ RTC_GUARDED_BY(network_thread());
+  absl::AnyInvocable<void() &&> on_first_packet_sent_
+      RTC_GUARDED_BY(network_thread());
 
   RtpTransportInternal* rtp_transport_ RTC_GUARDED_BY(network_thread()) =
       nullptr;
