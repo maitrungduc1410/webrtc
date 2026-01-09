faea246fadb3a0684e56943ac76f46192388bfec
Tomas Gunnarsson <tommi@webrtc.org>
2026-01-09 07:37:06 -0800

    Make SetMaximumBufferPackets and SetFastAccelerate pure virtual.
    Bug: webrtc:470860829
    Change-Id: I3afd683a7b075be0d347c2854858928f1c2f0a0d
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/441140
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Commit-Queue: Tomas Gunnarsson <tommi@webrtc.org>
    Reviewed-by: Jeremy Leconte <jleconte@google.com>
    Cr-Commit-Position: refs/heads/main@{#46605}

--- api/neteq/neteq.h
+++ api/neteq/neteq.h
@@ -250,11 +250,11 @@ class NetEq {
   virtual int RemovePayloadType(uint8_t rtp_payload_type) = 0;
 
   // Set the maximum number of packets to hold in the IO packet buffer.
-  virtual void SetMaximumBufferPackets(size_t max_packets) {}
+  virtual void SetMaximumBufferPackets(size_t max_packets) = 0;
 
   // Set if the FastAccelerate feature (accelerate based on arrival time) is
   // enabled.
-  virtual void SetFastAccelerate(bool enable) {}
+  virtual void SetFastAccelerate(bool enable) = 0;
 
   // Removes all payload types from the codec database.
   virtual void RemoveAllPayloadTypes() = 0;
