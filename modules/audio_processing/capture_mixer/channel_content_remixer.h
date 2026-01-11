2fe3d69473e92241e6a8ca28843254d47c78a3a2
Per Åhgren <peah@webrtc.org>
2026-01-11 16:58:36 +0100

    Adding code implementing the mixing logic for the capture signal remixer
    This is the fifth CL in the CL chain for adding the mixing capability
    for stereo signals. The function added in the CL will be used
    in the code added in the following CLs in the chain.
    
    Change-Id: I84bc31eee4af1a53ebf2e90454393d01385b997b
    Bug: webrtc:42221468
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/431181
    Reviewed-by: Tomas Lundqvist <tomasl@google.com>
    Commit-Queue: Per Åhgren <peah@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#46619}

---
+++ modules/audio_processing/capture_mixer/channel_content_remixer.h
@@ -0,0 +1,24 @@
+/*
+ *  Copyright (c) 2025 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+#ifndef MODULES_AUDIO_PROCESSING_CAPTURE_MIXER_CHANNEL_CONTENT_REMIXER_H_
+#define MODULES_AUDIO_PROCESSING_CAPTURE_MIXER_CHANNEL_CONTENT_REMIXER_H_
+
+namespace webrtc {
+
+enum class StereoMixingVariant {
+  kUseBothChannels,
+  kUseChannel0,
+  kUseChannel1,
+  kUseAverage
+};
+
+}  // namespace webrtc
+
+#endif  // MODULES_AUDIO_PROCESSING_CAPTURE_MIXER_CHANNEL_CONTENT_REMIXER_H_
