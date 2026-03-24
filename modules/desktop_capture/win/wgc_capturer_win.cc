1d5a6e111f7dc0d600c625a9ead6dd0a1b7219a9
Zhibo Wang <zhibo1.wang@intel.com>
2026-03-24 14:06:37 +0800

    Use GPU texture in desktop capture
    This change adds a FrameTexture member in the DesktopFrame class, so
    that WGC desktop capture can pass DXGIResource handles to supported
    frame consumers to save CPU-GPU copies.
    
    Bug: chromium:40929600
    Change-Id: Ide143596272e4b72c099afa2d366f58e5ef835c4
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/446781
    Reviewed-by: Henrik Andreassson <henrika@webrtc.org>
    Reviewed-by: Markus Handell <handellm@webrtc.org>
    Commit-Queue: Wang, Zhibo1 <zhibo1.wang@intel.com>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@webrtc.org>
    Reviewed-by: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#47254}

--- modules/desktop_capture/win/wgc_capturer_win.cc
+++ modules/desktop_capture/win/wgc_capturer_win.cc
@@ -431,7 +431,7 @@ void WgcCapturerWin::CaptureFrame() {
                             capture_time_ms);
   frame->set_capture_time_ms(capture_time_ms);
   frame->set_capturer_id(DesktopCapturerId::kWgcCapturerWin);
-  frame->set_may_contain_cursor(options_.prefer_cursor_embedded());
+  frame->set_may_contain_cursor(capture_session->MayContainCursor());
   frame->set_top_left(capture_source_->GetTopLeft());
   RecordWgcCapturerResult(WgcCapturerResult::kSuccess);
   callback_->OnCaptureResult(DesktopCapturer::Result::SUCCESS,
