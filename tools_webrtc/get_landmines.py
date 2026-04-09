abd2235574e1d787d79ac3e97438182a519ccd49
Jeremy Leconte <jleconte@webrtc.org>
2026-04-09 10:00:53 +0200

    Try to fix ('isolate tests') step failures.
    Failure is introduced with https://webrtc-review.git.corp.google.com/c/src/+/461480.
    
    Also remove host_os() == 'android' because the os for android is actually 'linux'.
    
    Change-Id: I7be263cb590763d50210c42e68b4c8fdcca3b63e
    Bug: webrtc:498394143
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/462480
    Reviewed-by: Philip Eliasson <philipel@webrtc.org>
    Commit-Queue: Jeremy Leconte <jleconte@google.com>
    Cr-Commit-Position: refs/heads/main@{#47358}

--- tools_webrtc/get_landmines.py
+++ tools_webrtc/get_landmines.py
@@ -35,6 +35,7 @@ def print_landmines():  # pylint: disable=invalid-name
     # landmine.
     # See the Chromium version in src/build/get_landmines.py for usage examples.
     print('Clobber to remove out/{Debug,Release}/args.gn (webrtc:5070)')
+    print('Clobber due to the rtc_test_suite migration (crbug.com/498394143)')
     if host_os() == 'win':
         print(
             'Clobber to resolve some issues with corrupt .pdb files on bots.')
@@ -71,8 +72,6 @@ def print_landmines():  # pylint: disable=invalid-name
         print('Lets clobber iOS due to signing issue b/396118151 (2nd try)')
         print('Lets clobber iOS due to rtc_executable -> rtc_test')
         print('Lets clobber iOS due to rtc_executable -> rtc_test')
-    if host_os() == 'android':
-        print('Clobber due to Android "compile confirm no-op" errors.')
 
 
 def main():
