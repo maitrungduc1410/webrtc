d57bda1bb6222d9606f683fdfc50f0fbe0033f8b
Harald Alvestrand <hta@webrtc.org>
2026-06-17 15:29:45 +0000

    Reland "Make RtpHeaderExtensionId(int) explicit"
    This reverts commit 031d798a20cc5d9c867598a38632accb20c6effe.
    
    Reason for revert: Revised constructor deprecation approach
    
    Original change's description:
    > Revert "Make RtpHeaderExtensionId(int) explicit"
    >
    > This reverts commit 9cb2a61b06bacaeb9a7abedc1d00517d7f79548d.
    >
    > Reason for revert: Suspected downstream failures.
    >
    > Failure Link: https://fusion2.corp.google.com/invocations/abc66f5c-f5bc-4767-8ff8-e67efeaa0f7c/targets/%2F%2Fbuzz%2Ftest%2Fpclf%2Ftests:absolute_capture_time_test_experiments_launched;config=a544fae14671853db631fbeeb923d84e665cd2045db88e266d17af360c6b5925/tests?q=status:failed%20OR%20status:broken%20OR%20status:cancelled%20OR%20status:running%20OR%20status:timed_out%20OR%20status:tool_failure%20label:buildbucket-8678846321400548705
    >
    > Original change's description:
    > > Make RtpHeaderExtensionId(int) explicit
    > >
    > > Make RtpHeaderExtensionId(int) constructor explicit to prevent
    > > implicit conversions. Added a deprecated template constructor to
    > > allow implicit conversion downstream to avoid breaking them. Also
    > > added explicit constructor for uint8_t to avoid matching the template
    > > in WebRTC.
    > >
    > > Bug: webrtc:514817938
    > > Change-Id: I5f8a03dc2ffb6d6d76164e734e1627f07727da49
    > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/482540
    > > Commit-Queue: Harald Alvestrand <hta@webrtc.org>
    > > Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    > > Cr-Commit-Position: refs/heads/main@{#48017}
    >
    > Bug: webrtc:514817938
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I367b804a5f3af448f1ce49b8f178a079edab1612
    > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/482800
    > Auto-Submit: Harald Alvestrand <hta@webrtc.org>
    > Commit-Queue: Jeremy Leconte <jleconte@google.com>
    > Reviewed-by: Jeremy Leconte <jleconte@google.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#48018}
    
    Bug: webrtc:514817938
    Change-Id: I476b96e2bdf869a27abe5dd46f4f67693d82d5cd
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/482820
    Commit-Queue: Harald Alvestrand <hta@webrtc.org>
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#48037}

--- call/payload_type_picker.cc
+++ call/payload_type_picker.cc
@@ -448,7 +448,8 @@ RTCErrorOr<RtpHeaderExtensionId> RtpHeaderExtensionPicker::SuggestMapping(
   // We prefer to allocate from the top of the range (14 down to 1).
   for (RtpHeaderExtensionId id =
            RtpHeaderExtensionId::kOneByteHeaderExtensionMaxId;
-       id >= RtpHeaderExtensionId::kMinId; id = id.value() - 1) {
+       id >= RtpHeaderExtensionId::kMinId;
+       id = RtpHeaderExtensionId(id.value() - 1)) {
     if (!seen_ids_.contains(id)) {
       AddMapping(id, uri, encrypt);
       return id;
