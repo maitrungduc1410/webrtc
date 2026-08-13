be9c40a7a9ef7c5485db5866410ef8ab66d88d8f
Danil Chapovalov <danilchap@webrtc.org>
2026-08-13 15:16:52 +0200

    Cleanup some unused AEC3 field trials
    These fields trials were already cleaned up from code
    
    Bug: webrtc:42221607
    Change-Id: Ib55456a68ecf5c3a535c763bc5de46bd1827c725
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/495400
    Reviewed-by: Per Åhgren <peah@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#48350}

--- experiments/field_trials.py
+++ experiments/field_trials.py
@@ -284,9 +284,6 @@ POLICY_EXEMPT_FIELD_TRIALS: FrozenSet[FieldTrial] = frozenset([
     FieldTrial('WebRTC-AdjustOpusBandwidth',
                42233664,
                date(2024, 4, 1)),
-    FieldTrial('WebRTC-Aec3AecStateFullResetKillSwitch',
-               42221578,
-               INDEFINITE),
     FieldTrial('WebRTC-Aec3AecStateSubtractorAnalyzerResetKillSwitch',
                42221578,
                INDEFINITE),
@@ -299,14 +296,8 @@ POLICY_EXEMPT_FIELD_TRIALS: FrozenSet[FieldTrial] = frozenset([
     FieldTrial('WebRTC-Aec3ClampInstQualityToZeroKillSwitch',
                42220991,
                INDEFINITE),
-    FieldTrial('WebRTC-Aec3CoarseFilterResetHangoverKillSwitch',
-               42222401,
-               date(2024, 4, 1)),
     FieldTrial('WebRTC-Aec3ConservativeTailFreqResponse',
                42223361,
-               date(2024, 4, 1)),
-    FieldTrial('WebRTC-Aec3DeactivateInitialStateResetKillSwitch',
-               42221578,
                date(2024, 4, 1)),
     FieldTrial('WebRTC-Aec3DelayEstimateSmoothingDelayFoundOverride',
                42222934,
@@ -385,9 +376,6 @@ POLICY_EXEMPT_FIELD_TRIALS: FrozenSet[FieldTrial] = frozenset([
     FieldTrial('WebRTC-Aec3SensitiveDominantNearendActivation',
                42221578,
                INDEFINITE),
-    FieldTrial('WebRTC-Aec3SetupSpecificDefaultConfigDefaultsKillSwitch',
-               42221236,
-               INDEFINITE),
     FieldTrial('WebRTC-Aec3ShortHeadroomKillSwitch',
                42220385,
                date(2024, 4, 1)),
@@ -456,9 +444,6 @@ POLICY_EXEMPT_FIELD_TRIALS: FrozenSet[FieldTrial] = frozenset([
                date(2024, 4, 1)),
     FieldTrial('WebRTC-Aec3TransparentModeHmm',
                42222401,
-               date(2024, 4, 1)),
-    FieldTrial('WebRTC-Aec3TransparentModeKillSwitch',
-               42234438,
                date(2024, 4, 1)),
     FieldTrial('WebRTC-Aec3Use1Dot2SecondsInitialStateDuration',
                42221578,
@@ -917,7 +902,7 @@ POLICY_EXEMPT_FIELD_TRIALS: FrozenSet[FieldTrial] = frozenset([
 ])  # yapf: disable
 
 POLICY_EXEMPT_FIELD_TRIALS_DIGEST: str = \
-    'c72a6c5ff92dac291461d264ebe2f7a234903131'
+    'b1d963f47bb9db11402bcea0a0d27fdfb13fd734'
 
 REGISTERED_FIELD_TRIALS: FrozenSet[FieldTrial] = ACTIVE_FIELD_TRIALS.union(
     POLICY_EXEMPT_FIELD_TRIALS)
