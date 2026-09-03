4e2f2ca44f00822a35ad62aafb78d564bb171dc4
webrtc-version-updater <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
2026-09-02 21:09:59 -0700

    Update WebRTC code version (2026-09-03T04:09:34).
    Bug: None
    Change-Id: I7b8a85d80c7e7d820a065f685689c3f9eb88ed5f
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/498881
    Bot-Commit: webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
    Commit-Queue: webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com <webrtc-version-updater@webrtc-ci.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#48478}

--- call/version.cc
+++ call/version.cc
@@ -13,7 +13,7 @@
 namespace webrtc {
 
 // The timestamp is always in UTC.
-const char* const kSourceTimestamp = "WebRTC source stamp 2026-09-02T04:06:29";
+const char* const kSourceTimestamp = "WebRTC source stamp 2026-09-03T04:09:34";
 
 void LoadWebRTCVersionInRegister() {
   // Using volatile to instruct the compiler to not optimize `p` away even
