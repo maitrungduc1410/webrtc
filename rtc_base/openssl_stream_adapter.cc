ad898d45fa2f4df2c30dd4312c59fa8c32a2c372
Philipp Hancke <philipp.hancke@googlemail.com>
2026-09-16 11:10:52 +0200

    ssl: use SSL_AD_INTERNAL_ERROR on timeout instead of -1
    Bug: webrtc:562024415
    Change-Id: Ifa328f60ffc0a3d55d703c2d3aa8b1a27bd41f43
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/503660
    Reviewed-by: Guido Urdaneta <guidou@webrtc.org>
    Reviewed-by: Johannes Kron <kron@webrtc.org>
    Reviewed-by: Danil Chapovalov <danilchap@webrtc.org>
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Cr-Commit-Position: refs/heads/main@{#48609}

--- rtc_base/openssl_stream_adapter.cc
+++ rtc_base/openssl_stream_adapter.cc
@@ -902,8 +902,7 @@ void OpenSSLStreamAdapter::SetTimeout(int delay_ms) {
             retransmission_count_++;
             RTC_LOG(LS_INFO) << "DTLS retransmission";
           } else if (res < 0) {
-            RTC_LOG(LS_INFO) << "DTLSv1_handle_timeout() return -1";
-            Error("DTLSv1_handle_timeout", res, -1, true);
+            Error("DTLSv1_handle_timeout", res, SSL_AD_INTERNAL_ERROR, true);
             return TimeDelta::PlusInfinity();
           }
           // We check the timer even after SSL_CONNECTED,
